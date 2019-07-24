13d653ee226b741a79f530f95e4020bbb0fe3357
trek-ash <yashsinghgreat@gmail.com>
2019-07-25 00:08:26 +0530

    user group extracted

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
 from flask import *
 from flask_oidc import OpenIDConnect
 from okta import UsersClient
+import jwt
+import requests
+from jwt.algorithms import RSAAlgorithm
+import json
 
 app = Flask(__name__)
 app.config["OIDC_CLIENT_SECRETS"] = "clients_secrets.json"
 app.config["OIDC_COOKIE_SECURE"] = False
 app.config["OIDC_CALLBACK_ROUTE"] = "/end/callback"
 app.config["OIDC_SCOPES"] = ["openid", "email", "profile"]
+app.config["OIDC_ID_TOKEN_COOKIE_NAME"]="oidc_token"
 app.config["SECRET_KEY"] = "{{ LONG_RANDOM_STRING }}"
 oidc = OpenIDConnect(app)
+
+
 
 okta_client = UsersClient("https://dev-764808.okta.com", "00eyvjQ5-edxVO-whsoxWbORgbfr7kc--8cuSckLzZ")
 
 
+def convert(data):
+    if isinstance(data, bytes):  return data.decode('ascii')
+    return data
 @app.before_request
 def before_request():
+    print(oidc.user_loggedin)
     if oidc.user_loggedin:
-        g.token = oidc.get_access_token()
-        g.user = okta_client.get_user(oidc.user_getfield("sub"))
+        if oidc.get_access_token() is None:
+            oidc.logout()
+        else:
+            g.user = okta_client.get_user(oidc.user_getfield("sub"))
+            access_token=oidc.get_access_token()
+            keys_respose=requests.get("https://dev-764808.okta.com/oauth2/default/v1/keys").content
+            keys_respose=convert(keys_respose)
+            keys_respose=json.loads(keys_respose)
+            key_json = keys_respose['keys'][0];
+            aud= "api://default"
+            public_key = RSAAlgorithm.from_jwk(json.dumps(key_json))
+            decoded = jwt.decode(access_token, public_key,audience=aud, algorithms='RS256')
+            g.groups = decoded["groups"]
     else:
         g.user = None
 
@@ -25,17 +47,13 @@ def before_request():
 def index():
     return render_template("index.html")
 
-
 @app.route("/dashboard")
 def dashboard():
-
-    if oidc.user_loggedin:
-        print(oidc.user_getinfo('group'))
-        return render_template("dashboard.html")
-    else:
-        return redirect(url_for(".index"))
-
-
+    print(g.groups)
+    if 'applicant' in g.groups:
+        return render_template("applicant/applicant_dashboard.html")
+    elif 'recruiter' in g.groups:
+        return render_template("recruiter/recruiter_dashboard.html")
 
 @app.route("/login")
 @oidc.require_login
@@ -47,3 +65,5 @@ def login():
 def logout():
     oidc.logout()
     return redirect(url_for(".index"))
+
+# app.run(debug=True)
